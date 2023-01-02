ed7912acce324a187723498a6575c2566e6e6251
mahmoud-sayed <mahmoudalsayied@gmail.com>
2023-01-02 16:35:01 +0200

    initail commit for the project

---
+++ src/GeneralImportantThings/colors.js
@@ -0,0 +1,17 @@
+const Colors = {
+  main: '#48B600',
+  white: '#fff',
+  black: '#000',
+  lightBlack: '#BCBCB',
+  orange: '#FFCE31',
+  red: '#F00',
+  gray: '#f4f4f4',
+  subGreen: '#EEFAE',
+  deepGray: '#E1F0D7',
+  deepestGray: '#C4C4C4',
+  blue: '#3B87F9',
+  underLine: '#E5E5E5',
+  paypal: '#FFB730'
+};
+
+export default colors;
---
+++ src/data/products.js
@@ -0,0 +1,45 @@
+const Products = [
+  {
+    _id: '1',
+    name: 'velcro ballerinas for girls (pink)',
+    image: 'https://res.cloudinary.com/zpune/image/upload/v1644396418/random/11_dzj0un.png',
+    description: 'lorem epsom is simply dummy text, lorem epsom is simply dummy text, lorem epsom is simply dummy text, lorem epsom is simply dummy text, lorem epsom is simply dummy text',
+    price: 89,
+    countInStuck: 3,
+    rating: 4,
+    numReviews: 4
+  },
+  {
+    _id: '2',
+    name: 'velcro ballerinas for girls (pink)',
+    image: 'https://res.cloudinary.com/zpune/image/upload/v1644396418/random/11_dzj0un.png',
+    description: 'lorem epsom is simply dummy text, lorem epsom is simply dummy text, lorem epsom is simply dummy text, lorem epsom is simply dummy text, lorem epsom is simply dummy text',
+    price: 89,
+    countInStuck: 3,
+    rating: 4,
+    numReviews: 4
+  },
+  {
+    _id: '3',
+    name: 'velcro ballerinas for girls (pink)',
+    image: 'https://res.cloudinary.com/zpune/image/upload/v1644396418/random/11_dzj0un.png',
+    description: 'lorem epsom is simply dummy text, lorem epsom is simply dummy text, lorem epsom is simply dummy text, lorem epsom is simply dummy text, lorem epsom is simply dummy text',
+    price: 89,
+    countInStuck: 3,
+    rating: 4,
+    numReviews: 4
+  },
+  {
+    _id: '4',
+    name: 'velcro ballerinas for girls (pink)',
+    image: 'https://res.cloudinary.com/zpune/image/upload/v1644396418/random/11_dzj0un.png',
+    description: 'lorem epsom is simply dummy text, lorem epsom is simply dummy text, lorem epsom is simply dummy text, lorem epsom is simply dummy text, lorem epsom is simply dummy text',
+    price: 89,
+    countInStuck: 3,
+    rating: 4,
+    numReviews: 4
+  },
+
+];
+
+export default Products;
